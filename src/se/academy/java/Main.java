1c50957ca1377e3e078910ab76ffa4b0cf7eb99b
joonsson <joonsson@gmail.com>
2017-10-09 13:02:05 +0200

    Added metroid

--- src/se/academy/java/Main.java
+++ src/se/academy/java/Main.java
@@ -12,12 +12,15 @@ public class Main {
         //MP3Player m = new MP3Player();
         MusicPlayer mVictory = new MusicPlayer("victory.mp3");
         MusicPlayer mBlues = new MusicPlayer("Blues-Loop.mp3", true);
+        MusicPlayer metroid = new MusicPlayer("Brinstar.mp3");
         MusicPlayer button = new MusicPlayer("button-3.mp3");
         MusicPlayer.musicPlayers.add(mVictory);
         MusicPlayer.musicPlayers.add(mBlues);
+        MusicPlayer.musicPlayers.add(metroid);
         MusicPlayer.musicPlayers.add(button);
         mVictory.start();
         mBlues.start();
+        metroid.start();
         button.start();
 
         Scanner sc = new Scanner(System.in);
@@ -26,9 +29,11 @@ public class Main {
         System.out.println("1\tPlay tune (one time)");
         System.out.println("2\tPlay tune (loop)");
         System.out.println("3\tStop tune (the loop)");
-        System.out.println("4\tStop all tunes");
-        System.out.println("5\tPause all tunes");
-        System.out.println("6\tResume all tunes (does not work, will start over all looped tunes)");
+        System.out.println("4\tPlay tune (Metroid)");
+        System.out.println("5\tStop tune (Metroid)");
+        System.out.println("6\tStop all tunes");
+        System.out.println("7\tPause all tunes");
+        System.out.println("8\tResume all tunes (does not work, will start over all looped tunes)");
         System.out.println("Default: Play a sound (try smashing ENTER)");
         do {
             System.out.print("\nCommand >> ");
@@ -52,13 +57,19 @@ public class Main {
                     mBlues.stopMusic();
                     break;
                 case "4":
+                    metroid.playMusic();
+                    break;
+                case "5":
+                    metroid.stopMusic();
+                    break;
+                case "6":
                     //m.stopAllTunes();
                     MusicPlayer.stopAllMusic();
-                case "5":
+                case "7":
                     //m.pauseAllTunes();
                     MusicPlayer.pauseAllMusic();
                     break;
-                case "6":
+                case "8":
                     //m.resumeAllTunes();
                     MusicPlayer.resumeAllMusic();
                     break;
